980ada45eeda033526b39dc503bdac8ed2fd04ca
Liliana <liliana.perezh.05@gmail.com>
2023-12-26 13:12:39 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://2f082bbc-3d20-49af-96a5-7de521508b5b.serverhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
